6fb4c6f9457e6e058c5212b46b03d5c08e59c4c4
mattyllier <bloodrootfox@gmail.com>
2023-03-07 15:25:00 -0600

    completed-variables-homework

---
+++ 1-variables/assignments/variables-homework.js
@@ -0,0 +1,25 @@
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+let age = 8;
+const height = "5 foot 9";
+const nickName = "Taom";
+const genus = "species G";
+const birthYear = 2014;
+const dob = "11-14";
+const animalInfo = [age,height,nickName,genus,birthYear,dob]
+console.log(animalInfo)
+
+
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+const colour = "green";
+let mileage = 15759;
+const plate = "SLD Y48";
+const expMonth = 8;
+let previousOwners = ["T.Avi", "Kevin S."];
+const engine = "v6";
+const carSpecs = [colour,mileage,plate,expMonth,previousOwners,engine]
+console.log(carSpecs)

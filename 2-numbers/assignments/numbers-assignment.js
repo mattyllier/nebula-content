8c751531fdd6eddeaf14be613f8f9ba5fada4b17
mattyllier <bloodrootfox@gmail.com>
2023-03-08 15:15:26 -0600

    numbers-assigment

---
+++ 2-numbers/assignments/numbers-assignment.js
@@ -0,0 +1,73 @@
+// Remeber to console.log() to debug your code and check your solutions!
+// Declare a variable and assign it to a numeric value
+let miNum = 59;
+console.log(miNum);
+// Half that value and re-assign it to the previous variable 
+miNum /= 2;
+console.log(miNum);
+// Create two negative numbers, assign them to variables and subtract one from the other together 
+const miNum0 = -31;
+const miNum1 = -33;
+console.log(miNum0-miNum1);
+
+// Create three numbers, assign them to variables and multiply them together 
+const miNum2 = 34;
+const miNum3 = 56;
+const miNum4 = 64;
+console.log(miNum2*miNum3*miNum4);
+
+// Create two even numbers, assign them to variables and divide one from the other 
+const miNum5 = 880;
+const miNum6 = 748;
+console.log(miNum5/miNum6);
+
+// Create two odd numbers, assign them to variables and find the remainder of dividing one by the other 
+const miNum7 = 69;
+const miNum8 = 1039;
+console.log(miNum8 % miNum7);
+
+// find the minimum value in a set of numbers: 10,20,30,40,50 
+const miVal = Math.min(10,20,30,40,50);
+console.log(miVal);
+//or
+console.log(Math.min(10,20,30,40,50))
+
+// find the maximum value in a set of numbers: -1,-2,-3,-4,-5 
+const miVal0 = Math.max(-1,-2,-3,-4,-5);
+console.log(miVal0);
+//or
+console.log(Math.max(-1,-2,-3,-4,-5));
+
+// Create a variable and find it's remainder, if divided by 2
+const myVal1 = 89;
+console.log(myVal1 % 2);
+
+// find the square root of 144 
+const num1 = 144;
+console.log(Math.sqrt(num1));
+//or
+console.log(Math.sqrt(144));
+
+// find the value of 3 to the power of 7 
+const myVal2 = 3;
+console.log(Math.pow(myVal2,7));
+//or
+console.log(Math.pow(3,7));
+
+// find the value of 73.2 rounded 
+const myVal3 = 73.2;
+console.log(Math.round(myVal3));
+//or
+console.log(Math.round(73.2));
+
+// find the rounded-down value of 9.9 
+const myVal4 = 9.9;
+console.log(Math.floor(myVal4));
+//or
+console.log(Math.floor(9.9));
+
+// find the absolute value of -1231
+const myVal6 = -1231;
+console.log(Math.abs(myVal6));
+//or
+console.log(Math.abs(-1231));

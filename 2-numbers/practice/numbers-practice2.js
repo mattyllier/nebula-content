49ff3ac914dc7f00d1461afbd0df6b69d33f069e
mattyllier <bloodrootfox@gmail.com>
2023-03-08 10:50:17 -0600

    numbers-practice2

---
+++ 2-numbers/practice/numbers-practice2.js
@@ -0,0 +1,53 @@
+// For these questions use the Math methods that JavaScript includes
+// All Math methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+// find the square root of 25
+const miNum = 25
+console.log(Math.sqrt(miNum))
+//or
+console.log(Math.sqrt(25))
+
+// find the value of 3 to the power of 5
+const miNum0 = Math.pow(3,5)
+console.log(miNum0)
+//or
+console.log(Math.pow(3,5))
+
+// find the value of 7.4 rounded
+const miNum1 = 7.4
+console.log(Math.round(miNum1))
+//or
+console.log(Math.round(7.4))
+
+// find the rounded-up value of 7.4
+console.log(Math.ceil(miNum1))
+//or
+console.log(Math.ceil(7.4))
+
+// find the absoloute value of -13
+const miNum2 = -13
+console.log(Math.abs(miNum2))
+//or
+console.log(Math.abs(-13))
+
+// find the minimum value in a set of numbers: 12,13,14,15,2,-1
+const myList = (12,13,14,15,2,-1)
+console.log(Math.min(myList))
+//or\r
+console.log(Math.min(12,13,14,15,2,-1))
+
+// find the maximum value in a set of numbers: 12,13,14,15,2,-1
+console.log(Math.min(myList))
+//or
+console.log(Math.max(12,13,14,15,2,-1))
+
+// create a random number 
+const randNum = Math.random()
+console.log(randNum)
+//or
+console.log(Math.random())
+
+// Create a random number between 0-10
+const randVal = Math.random(0,10)
+console.log(randVal)
+//or
+console.log(Math.random(0,10))

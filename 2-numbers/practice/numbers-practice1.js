0b73471d9561a7edb67e0634c7c8e36114eeae03
mattyllier <bloodrootfox@gmail.com>
2023-03-08 09:54:16 -0600

    numbers-practice

---
+++ 2-numbers/practice/numbers-practice1.js
@@ -0,0 +1,33 @@
+// Declare a variable and assign it to a numeric value
+// Double that value and re-assign it to the previous variable
+const miNum = 59
+console.log(miNum * 2)
+
+// Create two numbers, assign them to variables and add them together
+const miNum0 = 810
+const miNum1 = 759
+console.log(miNum0+miNum1)
+
+// Create three numbers, assign them to variables and subtract the smaller two from the largest
+const miNum2 = 61
+const miNum3 = 53
+const miNum4 = 64
+const minNum = Math.min(miNum2, miNum3, miNum4)
+console.log((miNum2 + miNum3 + miNum4)-minNum)
+
+// Create two even numbers, assign them to variables and divide one from the other
+const miNum5 = 8
+const miNum6 = 66
+console.log(miNum6/miNum5)
+
+// Create two odd numbers, assign them to variables and multiply one from the other
+const miNum7 = 31
+const miNum8 = 79
+console.log(miNum7*miNum8)
+
+// Create a variable and log it's remainder, if divided by 3, to the console 
+const miNum11 = 43
+console.log(miNum11 % 3)
+
+// Answer in plain text, what is modulo especially useful for?
+// '% 2' determines whether the number is odd or even

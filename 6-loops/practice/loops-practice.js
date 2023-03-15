75d2c909f20b9c8bc1f0a41bf1c3fda64f0ce42a
mattyllier <bloodrootfox@gmail.com>
2023-03-15 13:53:58 -0500

    updated-loops-practice

--- 6-loops/practice/loops-practice.js
+++ 6-loops/practice/loops-practice.js
@@ -133,3 +133,50 @@ console.log(count)
 //     }
 // }
 
+// 1.   Given an array of strings
+//      Loop over the array of strings
+//      And loop over each string.
+//      If a word has the letter "a" in it, log it to the console
+const names = ['Bill', 'Jordan', 'Pete', 'Daniel']
+for(let i = 0; i < names.length; i++){
+    for(let j = names[i]; j < names[i].length; j++){
+        if (j.includes('a')){
+            console.log(j)
+        }
+    }
+}
+//not logging 
+
+
+
+// 2.   Given an array of strings
+//      Loop over the array
+//      And loop over each string.
+//      Create a new array of strings where every other letter in each string is capitalized 
+//      Ex: ['This','is','an','array','of','strings'] => ['ThIs','Is','An','ArRaY','Of','StRiNgS']
+const stringArray = ['This','is','an','array','of','strings']
+const newArr1 = []
+for(let i = 0; i < stringArray.length; i++){
+    for(let j = stringArray[i]; j < stringArray[i].length; j++){
+        if (j % 2 === 0){
+            newArr1.push(j.toUpperCase)
+        } else{
+            newArr1.push(j)
+        }
+    }
+}
+console.log(newArr1)
+//not logging
+
+// 3.   Given an array of arrays of numbers (nested array)
+//      Add all the inner values up and push them into a single array
+//      Example: [[5, 4, 3], [20, 15, 50], [20, 13, 50], [21, 35, 10], [25, 21, 31]] => [12, 85, 83, 66, 77]
+const arrayOfArrays = [[5, 4, 3], [20, 15, 50], [20, 13, 50], [21, 35, 10], [25, 21, 31]]
+const values = []
+for(let i = 0; i < arrayOfArrays.length; i++){
+    for(let j = arrayOfArrays[i]; j < arrayOfArrays[i].length; j+=j){
+        values.push(j)
+    }
+}
+console.log(values)
+//not logging

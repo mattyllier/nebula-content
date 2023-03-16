b3934cb963d4b6b76cafe230d8bc5cf57f26d0a8
mattyllier <bloodrootfox@gmail.com>
2023-03-16 12:04:35 -0500

    compiled-loops-practice

--- 6-loops/practice/loops-practice.js
+++ 6-loops/practice/loops-practice.js
@@ -78,6 +78,8 @@ console.log()
 console.log()
 
 
+
+
 //while loop
 let i = 0
 while(i < 10){
@@ -114,7 +116,6 @@ while(i < myStr.length){
     i++
 }
 
-
 // 2.   Write a while loop that logs to the console the 'count' until the 'count' reaches 100
 //      Inside the while loop be sure to increase count by some number
 console.log()
@@ -124,14 +125,76 @@ while(count < 100 ){
 }
 console.log(count)
 
+console.log()
+
+
+
+
+//continue
+let numeri = ''
+for(let i = 0; i < 12; i++){
+    if(i===3 || i===5 || i===9){
+        continue;
+    }
+    numeri += i
+}
+console.log(numeri);
+console.log();
+
+// 1.   Write a for loop that looks for the letter a
+//      The loop should log the letter 'a' to the console
+//      STOP immediately after finding the letter 'a'
+const str = 'this string contains a hidden value';
+for(let i = 0; i < str.length; i++){
+    if(str[i] === 'a'){
+        console.log('a',[i])
+        break;
+    }
+}
+
+// 2.   Write a for loop that looks for a number
+//      The loop should log 'at index _ there is the number _' to the console 
+//      Stop immediately after finding that number
+//      ex: Input: ['a','b','c', 100,'e']
+//          Output: 'at index 3 there is the number 100'
+const numArr = [-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,86,722,815,1223]
+for(let i = 0; i < numArr.length; i++){
+    if(numArr[i] === 13){
+        console.log(`at index ${i} there is the number ${numArr[i]}`)
+    }
+}
+
+// 3.   Using the continue keyword loop over an array of numbers & strings
+//      If the current element is a number skip it
+//      Otherwise add the element to an array
+//      After finishing the loop log the new array to the console
+const numStrArray = ['a','e',1,'b','v',2,'g',3,'d',4,'h',5,'u','v','w',6,'z',7,'c','h',8,'t',9,'i','j','y',10,'k',20,'l',30,'m',40,'n',50,'s',60,'a','e',70,'p','f',80,'t','z',90,'q',100,'r',200,'s','h',300,'t',400]
+const lettersArray = []
+for(let i = 0; i < numStrArray.length; i++){
+    if(typeof(numStrArray[i])=== 'number'){
+        continue;
+    }
+    lettersArray.push(numStrArray[i])
+}
+console.log(lettersArray);
+
+console.log()
+
+
+
 //nested loop
 
-// const arr = ['a','b','c','f','a','l','c']
-// for(let i = 0; i < arr.length; i++){
-//     for(let j = i+1; j < arr.length; j++){
-//         console.log(arr[i],arr[j]);
-//     }
-// }
+const quote = "use quotes containing vowels"
+const vowels = ['a','e','i','o','u','A','E','I','O','U']
+const quoteVowels = []
+for(let i = 0; i < quote.length; i++){
+    for(let j = 0; j < vowels.length; j++){
+        if(quote[i]===vowels[j]){
+            quoteVowels.push(quote[i])
+        }
+    }
+}
+console.log(quoteVowels)
 
 // 1.   Given an array of strings
 //      Loop over the array of strings
@@ -139,15 +202,12 @@ console.log(count)
 //      If a word has the letter "a" in it, log it to the console
 const names = ['Bill', 'Jordan', 'Pete', 'Daniel']
 for(let i = 0; i < names.length; i++){
-    for(let j = names[i]; j < names[i].length; j++){
-        if (j.includes('a')){
-            console.log(j)
+    for(let j = 0; j < names[i].length; j++){
+        if (names[i][j]==='a' || names[i][j] ==='A'){
+            console.log(`'a' in ${names[i]}`)
         }
     }
 }
-//not logging 
-
-
 
 // 2.   Given an array of strings
 //      Loop over the array
@@ -155,28 +215,43 @@ for(let i = 0; i < names.length; i++){
 //      Create a new array of strings where every other letter in each string is capitalized 
 //      Ex: ['This','is','an','array','of','strings'] => ['ThIs','Is','An','ArRaY','Of','StRiNgS']
 const stringArray = ['This','is','an','array','of','strings']
-const newArr1 = []
+const capitalArr = []
 for(let i = 0; i < stringArray.length; i++){
-    for(let j = stringArray[i]; j < stringArray[i].length; j++){
-        if (j % 2 === 0){
-            newArr1.push(j.toUpperCase)
-        } else{
-            newArr1.push(j)
+    let newStr = ''   //assigned within i so it reassigns at each str
+    for(let j = 0; j < stringArray[i].length; j++){
+        if(j%2===0){
+        newStr += stringArray[i][j].toUpperCase()
+        } else {
+            newStr += stringArray[i][j]
         }
     }
+    capitalArr.push(newStr)
 }
-console.log(newArr1)
-//not logging
+console.log(capitalArr)
+
 
 // 3.   Given an array of arrays of numbers (nested array)
 //      Add all the inner values up and push them into a single array
 //      Example: [[5, 4, 3], [20, 15, 50], [20, 13, 50], [21, 35, 10], [25, 21, 31]] => [12, 85, 83, 66, 77]
 const arrayOfArrays = [[5, 4, 3], [20, 15, 50], [20, 13, 50], [21, 35, 10], [25, 21, 31]]
-const values = []
+const singleArray = []
+let addedValue = null
 for(let i = 0; i < arrayOfArrays.length; i++){
-    for(let j = arrayOfArrays[i]; j < arrayOfArrays[i].length; j+=j){
-        values.push(j)
+    for(let j = 0; j < arrayOfArrays[i].length; j++){
+      addedValue = arrayOfArrays[i][0]+arrayOfArrays[i][j]+ arrayOfArrays[i][j-1]   // += operates same way
     }
+    singleArray.push(addedValue)
 }
-console.log(values)
-//not logging
+console.log(singleArray)
+
+//or (better way)
+
+const singleArr = []
+for(let i = 0; i < arrayOfArrays.length; i++){
+    let addedVal = null  // assigned within i so it repeats at each
+    for(let j = 0; j < arrayOfArrays[1].length; j++){
+        addedVal += arrayOfArrays[i][j]
+    }
+    singleArr.push(addedVal)
+}
+console.log(singleArr)
